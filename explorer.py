9f854fda81ec00d25b9f8a4e393225444ed4c999
Thiago Aguiar <thiago.y4@hotmail.com>
2023-09-20 17:46:13 -0300

    Added more explorers.

--- explorer.py
+++ explorer.py
@@ -19,32 +19,32 @@ class Explorer(AbstractAgent):
         """
 
         super().__init__(env, config_file)
-        
+
         # Specific initialization for the rescuer
         self.resc = resc           # reference to the rescuer agent
-        self.rtime = self.TLIM     # remaining time to explore     
+        self.rtime = self.TLIM     # remaining time to explore
 
-   
-    
+
     def deliberate(self) -> bool:
         """ The agent chooses the next action. The simulator calls this
         method at each cycle. Must be implemented in every agent"""
 
         # No more actions, time almost ended
-        if self.rtime < 10.0: 
+        if self.rtime < 10.0:
             # time to wake up the rescuer
             # pass the walls and the victims (here, they're empty)
             print(f"{self.NAME} I believe I've remaining time of {self.rtime:.1f}")
-            self.resc.go_save_victims([],[])
+            # self.resc.go_save_victims([],[])
+            self.resc.body.set_state(PhysAgent.ENDED)                                  ##
             return False
-        
+
         dx = random.choice([-1, 0, 1])
 
         if dx == 0:
            dy = random.choice([-1, 1])
         else:
            dy = random.choice([-1, 0, 1])
-        
+
         # Check the neighborhood obstacles
         obstacles = self.body.check_obstacles()
 
@@ -72,6 +72,6 @@ class Explorer(AbstractAgent):
                 self.rtime -= self.COST_READ
                 # print("exp: read vital signals of " + str(seq))
                 # print(vs)
-                
+
         return True
 
--- main.py
+++ main.py
@@ -9,37 +9,44 @@ from explorer import Explorer
 from rescuer import Rescuer
 
 def main(data_folder_name):
-   
+
     # Set the path to config files and data files for the environment
     current_folder = os.path.abspath(os.getcwd())
     data_folder = os.path.abspath(os.path.join(current_folder, data_folder_name))
 
-    
+
     # Instantiate the environment
     env = Env(data_folder)
-    
+
     # config files for the agents
     rescuer_file = os.path.join(data_folder, "rescuer_config.txt")
-    explorer_file = os.path.join(data_folder, "explorer_config.txt")
-    
+    explorer_file = []
+    explorer_file.append(os.path.join(data_folder, "explorer0_config.txt"))
+    explorer_file.append(os.path.join(data_folder, "explorer1_config.txt"))
+    explorer_file.append(os.path.join(data_folder, "explorer2_config.txt"))
+    explorer_file.append(os.path.join(data_folder, "explorer3_config.txt"))
+
     # Instantiate agents rescuer and explorer
     resc = Rescuer(env, rescuer_file)
 
     # Explorer needs to know rescuer to send the map
     # that's why rescuer is instatiated before
-    exp = Explorer(env, explorer_file, resc)
+    exp0 = Explorer(env, explorer_file[0], resc)
+    exp1 = Explorer(env, explorer_file[1], resc)
+    exp2 = Explorer(env, explorer_file[2], resc)
+    exp3 = Explorer(env, explorer_file[3], resc)
 
     # Run the environment simulator
     env.run()
-    
-        
+
+
 if __name__ == '__main__':
     """ To get data from a different folder than the default called data
     pass it by the argument line"""
-    
+
     if len(sys.argv) > 1:
         data_folder_name = sys.argv[1]
     else:
         data_folder_name = os.path.join("datasets", "data_12x12_10vic")
-        
+
     main(data_folder_name)
